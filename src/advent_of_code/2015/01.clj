37c066ceb7dbc80fd6a3e1d3108fa5e341b49edb
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2016-11-11 12:54:21 +0000

    Some boilerplate out of the way

---
+++ src/advent_of_code/2015/01.clj
@@ -0,0 +1,8 @@
+(ns advent-of-code.2015.01)
+
+
+(defn solve1 []
+  (println "first problem"))
+
+(defn solve2 []
+  (println "second problem"))
---
+++ src/advent_of_code/core.clj
@@ -0,0 +1,22 @@
+(ns advent-of-code.core
+  (:gen-class)
+  (:require [advent-of-code.2015.01 :as P01]))
+
+(defn solve [day problemNum]
+  (let [d (read-string day),
+        p (read-string problemNum)]
+
+    (case (+ (* 10 d) p)
+      11 (P01/solve1)
+      12 (P01/solve2)
+      )
+    )
+  )
+
+(defn printUsage []
+  (println "Usage: <progname> <day> <1|2>"))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (if (< (count args) 2) (printUsage) (solve (nth args 0) (nth args 1))))
