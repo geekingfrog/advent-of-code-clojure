967dcd9beea0817ae027cb1bc5ba749a6eec4d6f
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2016-11-14 10:24:26 +0000

    destructuring

--- src/advent_of_code/2015/01.clj
+++ src/advent_of_code/2015/01.clj
@@ -3,10 +3,8 @@
 (defn read-file []
   (slurp "resources/2015/day01.txt"))
 
-(defn track-floors [acc c]
-  (let [current-floor (first acc)
-       idx (second acc)
-        next-idx (+ idx 1)]
+(defn track-floors [[current-floor idx] c]
+  (let [next-idx (+ idx 1)]
     (if (= \( c)
       (identity [(+ current-floor 1) next-idx])
       (identity [(- current-floor 1) next-idx]))
@@ -15,7 +13,6 @@
 (defn solve1 []
   (let [content (read-file)
         final-state (reduce track-floors [0 0] content)]
-    ;; (println (reduce track-floors (identity [0 0]) content))
     (println (first final-state))
     ))
 
