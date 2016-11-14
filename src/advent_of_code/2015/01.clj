e8a0dababa87cf55039037cb228ffd9d5e8a1e08
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2016-11-14 08:18:11 +0000

    Day 1

--- src/advent_of_code/2015/01.clj
+++ src/advent_of_code/2015/01.clj
@@ -1,8 +1,29 @@
 (ns advent-of-code.2015.01)
 
+(defn read-file []
+  (slurp "resources/2015/day01.txt"))
+
+(defn track-floors [acc c]
+  (let [current-floor (first acc)
+       idx (second acc)
+        next-idx (+ idx 1)]
+    (if (= \( c)
+      (identity [(+ current-floor 1) next-idx])
+      (identity [(- current-floor 1) next-idx]))
+  ))
 
 (defn solve1 []
-  (println "first problem"))
+  (let [content (read-file)
+        final-state (reduce track-floors [0 0] content)]
+    ;; (println (reduce track-floors (identity [0 0]) content))
+    (println (first final-state))
+    ))
 
 (defn solve2 []
-  (println "second problem"))
+  (let [content (read-file)
+        all-states (reductions track-floors [0 0] content)
+        filtered (filter #(= -1 (first %)) all-states)
+        ]
+    (println (second (first filtered)))
+    )
+  )
