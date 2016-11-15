6891828f9552baf416e97663d79d42f584eced2c
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2016-11-15 11:46:05 +0000

    Day 05 part 2

--- src/advent_of_code/2015/05.clj
+++ src/advent_of_code/2015/05.clj
@@ -1,6 +1,5 @@
 (ns advent-of-code.2015.05)
 
-;; (defn read-data [] (slurp "resources/2015/day05.txt"))
 (defn read-data []
   (with-open [rdr (clojure.java.io/reader "resources/2015/day05.txt")]
     (doall (line-seq rdr))))
@@ -39,4 +38,41 @@
   (let [content (read-data)]
     (println (count (filter good-string1? content)))))
 
-(defn solve2 [])
+
+;;;;;;;;;;;;;;;;;;;; day 2
+
+(defn all-pairs-idx [s]
+  (map list s (rest s) (range)))
+
+
+;; Big doubt about this thing, maybe loop/recur would be better here
+;; but I'm not sure how to iterate over a collection while having
+;; a state with loop/recur
+(defn repeating-pair? [s]
+  (let [pairs (all-pairs-idx s)
+        reducer (fn [[seen-pairs result] [a b idx]]
+                  (if (contains? seen-pairs [a b])
+                    (if (> idx (inc (seen-pairs [a b])))
+                       [seen-pairs true]
+                       [seen-pairs result])
+                    [(assoc seen-pairs [a b] idx) result]
+                    )
+                  )
+        [_ res] (reduce reducer [{} false] pairs)
+        ]
+    res
+    ))
+
+(defn intercalated-pair? [s]
+  (let [tuples (map list s (rest s) (drop 2 s))
+        filter-fn (fn [[a _ c]] (= a c))]
+    (boolean (not-empty (filter filter-fn tuples)))
+    ))
+
+(defn good-string2? [s]
+  (and (repeating-pair? s) (intercalated-pair? s)))
+
+(defn solve2 []
+  (let [content (read-data)]
+    (println (count (filter good-string2? content)))
+    ))
