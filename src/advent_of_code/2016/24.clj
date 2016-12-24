54287dcda27208408d05a5e706160da2353db83e
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2016-12-24 17:40:10 +0000

    2016 - day 24 (A* version)

--- src/advent_of_code/2016/24.clj
+++ src/advent_of_code/2016/24.clj
@@ -1,117 +1,6 @@
 (ns advent-of-code.2016.24
   (:require [clojure.string :as str]))
 
-(defn base-matrix [w h]
-  (loop [j 0 row (transient [])]
-    (if (>= j h)
-      (persistent! row)
-      (let [col (loop [i 0 c (transient [])]
-                  (if (>= i w)
-                    (persistent! c)
-                    (recur (inc i) (conj! c (if (= i j) 0 java.lang.Integer/MAX_VALUE)))
-                    ))
-            ]
-        (recur (inc j) (conj! row col))
-        )
-      )
-    )
-  )
-
-;; (defn graph-dist [g [i j i' j']]
-;;   (let [row (g j)
-;;         col (row i)
-;;         a ((g j) i)
-;;         b ((g j') i')
-;;         ]
-;;     (when (and (not= \# a) (not= \# b))
-;;       1
-;;       )
-;;     ))
-
-(defn nodes [g]
-  ;; ignore the borders of the graphs
-  (println "nodes g")
-  (let [h (count g)
-        w (count (first g))
-        ]
-    (do (for [i (range 1 (dec w))
-              j (range 1 (dec h))
-              :when (not= \# ((g j) i))
-              ]
-          (list i j)
-          )
-        )
-    )
-  )
-
-(defn dist [i j i' j']
-  (+ (Math/abs (- i' i)) (Math/abs (- j' j)))
-  )
-
-
-(defn graph-dist [g [i j] [i' j']]
-  (cond
-    (and (= i i') (= j j')) 0
-    (< (dist i j i' j') 2) 1
-    :else java.lang.Integer/MAX_VALUE
-  ))
-
-
-(defn floyd-warshall [g dists [k i j]]
-  (let [dij (dists (list i j))
-        dik (dists (list i k))
-        dkj (dists (list k j))
-        new-dist (+ dik dkj)
-        ]
-    (if (> dij new-dist)
-      (assoc dists (list i j) new-dist)
-      dists
-      )
-    )
-  )
-
-
-(defn base-dists [g]
-  (println "base dists")
-  (let [ns (nodes g)
-        idxs (do (for [i ns j ns ]
-                   (list i j)
-                   ))
-        ]
-    (println "base-dists, number of steps: " (count idxs))
-    (reduce #(assoc %1 %2 (apply graph-dist g %2)) {} idxs))
-  )
-
-
-(defn shortest-paths [g]
-  (let
-    [h (count g)
-     w (count (first g))
-     idxs (do (for [k (nodes g)
-                    i (nodes g)
-                    j (nodes g)
-                    ]
-                (list k i j)
-                ))
-     base (base-dists g)
-     ]
-    (println "number of steps: " (count idxs))
-    (reduce (partial floyd-warshall g) base idxs)
-    )
-  )
-
-(defn reduce-target [g m [i j]]
-  (let [a ((g j) i)]
-    (if (and (not= \. a) (not= \# a))
-      (assoc m a (list i j))
-      m
-      )
-    )
-  )
-
-(defn find-targets [g]
-  (reduce (partial reduce-target g) {} (nodes g)))
-
 ;; (def testarr
 ;;   [(vec "#####")
 ;;    (vec "#0..#")
@@ -129,12 +18,7 @@
    ]
   )
 
-(defn permutations [s]
-  (lazy-seq
-   (if (seq (rest s))
-     (apply concat (for [x s]
-                     (map #(cons x %) (permutations (remove #{x} s)))))
-     [s])))
+(defn dist [[i j] [i' j']] (+ (Math/abs (- i' i)) (Math/abs (- j' j))))
 
 (defn parse-data [path]
   (->>
@@ -144,38 +28,149 @@
     (vec)
     ))
 
-(defn path-length [dists targets path]
-  (let [tuples (map list path (rest path))
-        coords (map #(list (targets (first %)) (targets (second %))) tuples)
-        segments (map #(get dists %) coords)
+
+(defn permutations [s]
+  (lazy-seq
+   (if (seq (rest s))
+     (apply concat (for [x s]
+                     (map #(cons x %) (permutations (remove #{x} s)))))
+     [s])))
+
+
+(defn nodes [g]
+  ;; ignore the borders of the graphs
+  (let [h (count g)
+        w (count (first g))
         ]
-    ;; (println dists)
-    ;; (println targets)
-    ;; (println path)
-    ;; (println coords)
-    ;; (println segments)
-    (reduce + 0 segments)
+    (do (for [i (range 1 (dec w))
+              j (range 1 (dec h))
+              :when (not= \# ((g j) i))
+              ]
+          (list i j)
+          )
+        )
     )
   )
 
-(defn solve1 []
-  (let [graph (parse-data "resources/2016/day24.txt")
-        targets (find-targets graph)
-        dists (shortest-paths graph)
-        possible-paths (filter #(= \0 (first %)) (permutations (keys targets)))
-        all-lengths (map #(path-length dists targets %) possible-paths)
-        ]
 
-    (println all-lengths)
-    (println (apply min all-lengths))
+(defn reduce-target [g m [i j]]
+  (let [a ((g j) i)]
+    (if (and (not= \. a) (not= \# a))
+      (assoc m a (vector i j))
+      m
+      )
+    )
+  )
+
+(defn find-targets [g]
+  (reduce (partial reduce-target g) {} (nodes g)))
+
+(defn adjacent-nodes [g [i j] visited]
+  (->>
+    [[(dec i) j] [(inc i) j] [i (dec j)] [i (inc j)]]
+    (filter #(not= \# ((g (second %)) (first %))))
+    (filter #(not (contains? visited %)))
+    ))
+
+(defn should-add-to-openset? [open-set [n d]]
+  (or
+    (not (contains? open-set n))
+    (< d (open-set n))
+    ))
+
+
+(defn pretty-dir [prev cur]
+  (let [[i j] prev
+        [i' j'] cur
+        ]
+    (cond
+      (= (inc i) i') \→
+      (= (dec i) i') \←
+      (= (dec j) j') \↑
+      (= (inc j) j') \↓
+      )
+    )
+  )
+
+(defn reconstruct-path [path target]
+  (loop [current target prev (path target) acc '()]
+    (if (nil? prev)
+      acc
+      (recur prev (path prev) (conj acc (pretty-dir prev current)))
+    )
   ))
 
 
-(defn solve2 []
-  (let [graph (parse-data "resources/2016/day24.txt")]
-    (println "start")
-    (base-dists graph)
-    (println "done")
-    ))
+(defn shortest' [g source target]
+  "A* algo"
+  (loop
+    [open-set {source 0}
+     closed-set #{}
+     came-from {}
+     ]
 
-;; (defn solve2 [] (println "not yet"))
+    (when
+      (empty? open-set)
+      (throw (Exception. (str "No path from " source " to " target))))
+
+    (let [[n nscore] (apply min-key second open-set)
+          open-set' (dissoc open-set n)
+          ;; _ (println "openset: " open-set)
+          ;; _ (println "n " n)
+          ;; _ (println "closedset: " closed-set)
+          next-nodes (adjacent-nodes g n closed-set)
+          ;; _ (println "next nodes: " next-nodes)
+          next-scores (repeat (inc nscore))
+          next-open-nodes (map list next-nodes next-scores)
+          ;; _ (println "wtf ?" next-open-nodes)
+          filtered-nodes (filter #(should-add-to-openset? open-set %) next-open-nodes)
+          ;; _ (println filtered-nodes)
+          open-set'' (reduce #(apply assoc %1 %2) open-set' filtered-nodes)
+          came-from' (reduce #(assoc %1 (first %2) n) came-from filtered-nodes)
+          ]
+      (if (= n target)
+        (reconstruct-path came-from' target)
+        (recur
+          open-set''
+          (conj closed-set n)
+          came-from'
+          )
+        )
+      )
+    )
+  )
+
+(def shortest (memoize shortest'))
+
+(defn path-length [g targets stops]
+  (->>
+    (map list stops (rest stops))
+    (map #(list (targets (first %)) (targets (second %))))
+    (map #(apply shortest g %))
+    (map count)
+    (reduce + 0)
+    (#(vector % stops))
+    )
+  )
+
+
+(defn solve1 []
+  (let [graph (parse-data "./resources/2016/day24.txt")
+        targets (find-targets graph)
+        possible-stops (map #(conj % \0) (permutations (keys (dissoc targets \0))))
+        all-lengths (map #(path-length graph targets %) possible-stops)
+        ]
+    (println (apply min-key first all-lengths))
+    )
+  )
+
+(defn solve2 []
+  (let [graph (parse-data "./resources/2016/day24.txt")
+        targets (find-targets graph)
+        possible-stops (map #(conj % \0) (permutations (keys (dissoc targets \0))))
+        stops-with-returns (map #(concat %1 '(\0)) possible-stops)
+        all-lengths (map #(path-length graph targets %) stops-with-returns)
+        ]
+    (println (apply min-key first all-lengths))
+    )
+  )
